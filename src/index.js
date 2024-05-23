582ab3c7ee44edc57a4507810ea08dc8568bbd0c
magnav0k <sokolov.misha01@mail.ru>
2024-05-23 14:59:20 +0300

    fix index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import readlineSync from 'readline-sync';
 
 const countOfQuestions = 3;
 
-const greeting = (rule, getQuestionAndAnswer) => {
+export default (rule, getQuestionAndAnswer) => {
     console.log('Welcome to the Brain Games!');
     const name = readlineSync.question('May I have your name? ');
     console.log(`Hello, ${name}!`);
